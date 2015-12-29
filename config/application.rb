8edff12a02babc9fc4a2c2abcf77c8b29d759d39
gemma1028 <hideki.suzuki@social-bridge.com>
2015-12-29 20:04:06 +0000

    ranking

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.web_console.whitelisted_ips = '180.49.130.253'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
